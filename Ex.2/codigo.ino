f5a76acaa350e0dbee15cd1556c2d525e0834afb
spyziouva <99329668+spyziouva@users.noreply.github.com>
2022-03-16 08:32:57 -0300

    Create codigo.ino

---
+++ Ex.2/codigo.ino
@@ -0,0 +1,16 @@
+void setup()
+{
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(2, HIGH);
+  digitalWrite(3, LOW);
+  delay(1000);
+  digitalWrite(3, HIGH);
+  digitalWrite(2, LOW);
+
+  delay(1000);
+}
